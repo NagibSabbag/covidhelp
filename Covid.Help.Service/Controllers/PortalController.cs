6ce4d8f3cbaf45f93fa446f00de7add6bc3d836d
Nagib Sabbag Filho <na.sabbag@gmail.com>
2020-05-24 15:27:07 -0300

    .

---
+++ Covid.Help.Service/Controllers/PortalController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Covid.Help.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortalController : Controller
+    {
+        private static List<Person> person;
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Person>), 200)]
+        public async Task<IActionResult> GetAsync()
+        {
+            if (person == null)
+                  person = await Person.GetMock();
+
+            return  Json(person);
+        }
+        [HttpGet]
+        [Route("person/{id}")]
+        [ProducesResponseType(typeof(Person), 200)]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            if (person == null)
+                person = await Person.GetMock();
+
+            return Json(person.FirstOrDefault(a=> a.Id.ToString() == id));
+        }
+    }
+}
