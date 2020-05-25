3852d0223af1b828328bcc9a40f2b63989601d58
Nagib Sabbag Filho <na.sabbag@gmail.com>
2020-05-25 18:59:48 -0300

    .

--- Covid.Help.Service/Controllers/TwilioController.cs
+++ Covid.Help.Service/Controllers/TwilioController.cs
@@ -3,6 +3,9 @@ using Covid.Help.Models.Request;
 using Covid.Help.Models.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Covid.Help.Service.Controllers
 {
@@ -13,15 +16,19 @@ namespace Covid.Help.Service.Controllers
         [HttpPost]
         [Route("call")]
         [ProducesResponseType(typeof(TwilioApiResponse), StatusCodes.Status200OK)]
-        public string SetCall([FromBody]TwilioApiRequest twilioApiRequest)
+        public async Task<string> SetCall([FromForm]TwilioApiRequest twilioApiRequest)
         {
+            //var parameters = string.Empty;
+            //using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            //    parameters = await reader.ReadToEndAsync();
+
             var twilioResponse = new TwilioApiResponse
             {
                 Say = new TwilioSayApiResponse
                 {
                     Voice = "alice",
                     Language = "pt-BR",
-                    Value = twilioApiRequest.CallStatus + " Bom dia. Bom dia. Bom dia. Bom dia.Jair Messias Bolsonaro é um capitão reformado, político e atual presidente do Brasil. Foi deputado federal por sete mandatos entre 1991 e 2018, sendo eleito através de diferentes partidos ao longo de sua carreira. Elegeu-se à presidência pelo Partido Social Liberal."
+                    Value = twilioApiRequest.CalledCity + " Bom dia. Bom dia. Bom dia. Bom dia.Jair Messias Bolsonaro é um capitão reformado, político e atual presidente do Brasil. Foi deputado federal por sete mandatos entre 1991 e 2018, sendo eleito através de diferentes partidos ao longo de sua carreira. Elegeu-se à presidência pelo Partido Social Liberal."
                 }
             };
             var twilioXml = new TwilioApiMap(twilioResponse).ToXml();
