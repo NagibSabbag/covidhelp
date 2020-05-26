3b064408a33d6985850a1e7e5c8ea4649e3a3ea9
Nagib Sabbag Filho <na.sabbag@gmail.com>
2020-05-26 02:44:57 -0300

    .

--- Covid.Help.Service/Controllers/CallController.cs
+++ Covid.Help.Service/Controllers/CallController.cs
@@ -1,28 +1,29 @@
 ﻿using Covid.Help.Map;
-using Covid.Help.Models.Request;
 using Covid.Help.Models.Response;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
 
 namespace Covid.Help.Service.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CallController : Controller
+    public class CallController : TwilioController
     {
         [HttpPost]
         [Route("init")]
         [Produces("text/xml")]
         [ProducesResponseType(typeof(CallApiResponse), StatusCodes.Status200OK)]
-        public ContentResult InitCall(CallApiRequest callApiRequest)
+        public ContentResult InitCall(VoiceRequest voiceRequest)
         {
             var callApiResponse = new CallApiResponse
             {
                 Say = new CallSayApiResponse
                 {
                     Voice = "Polly.Camila-Neural",
-                    Value = callApiRequest.From + "Olá. Olá. Olá. Eu sou a Vivi, sua assistente virtual. Fui desenvolvida para ajudar na triagem dos casos do Corona vírus, assim como passar algumas informações que podem ser de grande ajuda. Espero ajudar de alguma forma."
+                    Value = voiceRequest.From + " Olá. Olá. Olá. Eu sou a Vivi, sua assistente virtual. Fui desenvolvida para ajudar na triagem dos casos do Corona vírus, assim como passar algumas informações que podem ser de grande ajuda. Espero ajudar de alguma forma."
                 }
             };
             var callXml = new CallApiMap(callApiResponse).ToXml();
