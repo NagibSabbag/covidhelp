ab6465baf71aa71ed2ec47e8a4f9bd60169f0b5e
Nagib Sabbag Filho <na.sabbag@gmail.com>
2020-05-26 02:31:04 -0300

    .

--- Covid.Help.Service/Controllers/CallController.cs
+++ Covid.Help.Service/Controllers/CallController.cs
@@ -22,7 +22,7 @@ namespace Covid.Help.Service.Controllers
                 Say = new CallSayApiResponse
                 {
                     Voice = "Polly.Camila-Neural",
-                    Value = callApiRequest.From + "Olá. Eu sou a Vivi, sua assistente virtual. Fui desenvolvida para ajudar na triagem dos casos do Corona vírus, assim como passar algumas informações que podem ser de grande ajuda. Espero ajudar de alguma forma."
+                    Value = callApiRequest.From + "Olá. Olá. Olá. Eu sou a Vivi, sua assistente virtual. Fui desenvolvida para ajudar na triagem dos casos do Corona vírus, assim como passar algumas informações que podem ser de grande ajuda. Espero ajudar de alguma forma."
                 }
             };
             var callXml = new CallApiMap(callApiResponse).ToXml();
